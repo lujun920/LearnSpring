5c14cb14af472a246cb5f951e43c09db0d964a28
lujun920 <lujun920@yeah.net>
2017-04-17 11:23:03 +0800

    重新上传消费者服务A

---
+++ cloud-provider/cloud-web/src/main/java/com.zt/model/User.java
@@ -0,0 +1,29 @@
+package com.zt.model;
+
+/**
+ * <p/>类文件: com.zt.model
+ * <p/>类功能描述: ${todo}
+ *
+ * @作者: luj
+ * @时间: 2017/4/13 2:02
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
