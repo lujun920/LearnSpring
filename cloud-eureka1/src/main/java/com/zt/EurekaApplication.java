49e1b09bb6899bdcfb3a5d83c8fa0e30c378dd6b
lujun920 <lujun920@yeah.net>
2017-04-22 23:09:37 +0800

    docker 打包编译配置

--- cloud-eureka1/src/main/java/com/zt/EurekaApplication.java
+++ cloud-eureka1/src/main/java/com/zt/EurekaApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication{
+public class EurekaApplication {
     public static void main(String[] args){
         new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
     }
